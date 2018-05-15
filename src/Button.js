35fd4a238f0fe21713decc994f8efbc48abb2110
nikolapejcic <nikola.pejcic.to@gmail.com>
2018-05-15 13:26:24 -0400

    Button and buttonset componet structured

--- src/Button.js
+++ src/Button.js
@@ -2,15 +2,145 @@ import React from "react";
 import styled from "styled-components";
 
 const Button = styled.button`
-  background-color: white;
-  border: 2px solid Coral;
-  color: Coral;
-  padding: 1rem 2rem;
-  border-radius: .4rem;
+// Type and vertical rythm baseline for the component
+  font-size: 1em;
+  line-height: 1.25;
+  // font-family
+  // Use shorthand
+
+// Spacing and sizing
+  padding: 1em 2em;
+  border: 1px solid transparent; // Use something generic for color
+  border-radius: .25rem;
+
+// Style
+  cursor: pointer;
+  // dropshadow, gradient
+
+// States
+  color: white;
+  background-color: lime;
+  border-color: lime;
   &:hover {
-    background-color: Coral;
-    color: white;
+    color: white; // white + 10
+    background-color: lime; // lime + 10
+    border-color: lime; // lime + 10
   }
+
+// Modifiers
+
+// Themes
+  &.button__base-theme{
+    color: white;
+    background-color: black;
+    border-color: black;
+
+    &:hover {
+      // darken 10%
+    }
+  }
+
+// Style / Function
+  &.button__primary{
+    color: white;
+    background-color: blue;
+    border-color: blue;
+
+    &:hover {
+      // darken 10%
+    }
+  }
+
+  &.button__secondary{
+    color: white;
+    background-color: gray;
+    border-color: gray;
+
+    &:hover {
+      // darken 10%
+    }
+  } // Neutral
+
+  &.button__plain{
+    color: black;
+    background-color: transparent;
+    border-color: transparent;
+
+    text-decoration: underline;
+
+    &:hover {
+      // darken 10%
+      text-decoration: none;
+    }
+  }
+
+  &.button__affirming{
+    color: white;
+    background-color: green;
+    border-color: green;
+    &:hover {
+      // darken 10%
+    }
+  }
+
+  &.button__destructive{
+    color: white;
+    background-color: red;
+    border-color: red;
+    &:hover {
+      // darken 10%
+    }
+  }
+
+  &.button__floated{
+    position: fixed;
+    bottom: 1em;
+    right: 1em;
+    z-index: 100; // highest level
+    //dropshadow: TBD;
+  }
+
+  // Status
+
+    &.button__disabled{
+      opacity: .5;
+    }
+
+    &.button__loading{ // responding / submiting
+    }
+
+// Structure
+  &.button__trigger{
+
+  }
+  &.button__iconic{
+
+  }
+
+// Size
+  &.button__full-width{
+
+  }
+
+  &.button__mini{
+
+  }
+
+  &.button__maxi{
+
+  }
+
+// Context
+  form &.button(__in-form){
+
+  }
+  table &.button(__in-table){
+
+  }
+  .modal &.button(__in-modal){
+
+  }
+
 `;
 
 export default Button;
--- src/Button.story.js
+++ src/Button.story.js
@@ -4,11 +4,46 @@ import { action } from '@storybook/addon-actions';
 import Button from "./Button";
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+  .add('Base', () => <Button className="button" onClick={action('clicked')}>Base-theme Button</Button>)
+
+storiesOf('Button/Themes', module)
+  .add('PackManger', () => <Button className="button__PackManager-theme" onClick={action('clicked')}>Base-theme Button</Button>)
+  .add('Go', () => <Button className="button__Go-theme" onClick={action('clicked')}>Base-theme Button</Button>)
+  .add('QCloud', () => <Button className="button__QCloud-theme" onClick={action('clicked')}>Base-theme Button</Button>)
+
+storiesOf('Button/Style | Function', module)
+  .add('Primary', () => <Button className="button__primary" onClick={action('clicked')}>Primary Base-theme Button</Button>)
+  .add('Secondary / Neutral', () => <Button className="button__secondary" onClick={action('clicked')}>Secondary Base-theme Button</Button>)
+  .add('Plain / Link', () => <Button className="button__plain" onClick={action('clicked')}>Plain Base-theme Button</Button>)
+  .add('Affirming', () => <Button className="button__affirming" onClick={action('clicked')}>Affirming Base-theme Button</Button>)
+  .add('Destructive', () => <Button className="button__destructive" onClick={action('clicked')}>Destructive Base-theme Button</Button>)
+  .add('Floated', () => <Button className="button__floated" onClick={action('clicked')}>Floated Base-theme Button</Button>)
+
+storiesOf('Button/Status', module)
+  .add('Disabled', () => <Button className="button__disabled" onClick={action('clicked')}>Disabled Base-theme Primary Button</Button>)
+  .add('Loading', () => <Button className="button__loading" onClick={action('clicked')}>Loading Base-theme Primary Button</Button>)
+
+storiesOf('Button/Size', module)
+  .add('Lean', () => <Button className="button__lean" onClick={action('clicked')}>Lean Base-theme Primary Button</Button>)
+  .add('Fat', () => <Button className="button__fat" onClick={action('clicked')}>Fat Base-theme Primary Button</Button>)
+  .add('Full-width', () => <Button className="button__full-width" onClick={action('clicked')}>Full-width Base-theme Primary Button</Button>)
+
+storiesOf('Button/Context', module)
+  .add('Form', () => <Button className="" onClick={action('clicked')}>Form Button</Button>)
+  .add('Table', () => <Button className="" onClick={action('clicked')}>Table Button</Button>)
+  .add('Modal', () => <Button className="" onClick={action('clicked')}>Modal Button</Button>)
+
+storiesOf('Buttonset', module)
+  .add('Inline', () =>
+    <section class="button-set__inline">
+      <Button className="button__primary" onClick={action('clicked')}>Primary Button</Button>
+      <Button className="button__secondary" onClick={action('clicked')}>Secondary Button</Button>
+      <Button className="button__secondary" onClick={action('clicked')}>Secondary Button</Button>
+      <Button className="button__secondary button__disabled" onClick={action('clicked')}>Disabled Button</Button>
+  </section>)
+  .add('Triggered', () => <section class="button-set__triggered">
+      <Button className="button__primary" onClick={action('clicked')}>Primary Button</Button>
+      <Button className="button__secondary" onClick={action('clicked')}>Secondary Button</Button>
+      <Button className="button__secondary" onClick={action('clicked')}>Secondary Button</Button>
+      <Button className="button__secondary button__disabled" onClick={action('clicked')}>Disabled Button</Button>
+  </section>)
